d1c9bfe1b0b5731bfff20d837386b5ba4c502378
Christopher Kirkwood <christopherkirkwood@Christophers-MacBook-Air.local>
2021-07-25 10:29:30 -0700

    adding update

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -4,6 +4,21 @@ class PuppiesController < ApplicationController
     render json: puppies.as_json
   end
   def create
-    
+    puppy = Puppy.new(
+      name: params[:name],
+      age: params[:age],
+      breed: params[:breed],
+    )
+    puppy.save
+    puppies = Puppy.all
+    render json: puppies.as_json
+  end
+  def update
+    puppy = Puppy.find_by(id: params[:id])
+    puppy.name = params[:name]
+    puppy.age = params[:age]
+    puppy.breed = params[:breed]
+    puppy.save
+    render json: puppy.as_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/puppies" => "puppies#index"
+  post "/puppies" => "puppies#create"
+  patch "/puppies/:id" => "puppies#update"
 end
